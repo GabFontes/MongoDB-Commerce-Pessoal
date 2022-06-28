e29ea5528147b93920c75f0c8daf08a9a43ee8f6
gab <gabrielfontes321@gmail.com>
2022-06-28 00:38:17 -0300

    feat: implementing challenge 17

---
+++ challenges/desafio17.js
@@ -0,0 +1,17 @@
+db.createCollection("resumoProdutos");
+db.resumoProdutos.insertMany(
+  [
+    {
+      franquia: "McDonalds",
+      totalProdutos: db.produtos.countDocuments({}),
+    },
+  ],
+);
+db.resumoProdutos.find(
+  {},
+  {
+    franquia: 1,
+    totalProdutos: 1,
+    _id: 0,
+  },
+);
